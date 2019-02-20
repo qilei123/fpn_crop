e8f71732a41cae9495bc326fac4773ef28681290
qilei123 <qileimail123@gmail.com>
2019-02-20 02:03:05 -0500

    fdaf

--- fpn/core/loader.py
+++ fpn/core/loader.py
@@ -200,7 +200,8 @@ class PyramidAnchorIterator(mx.io.DataIter):
 
     @property
     def provide_data(self):
-        print 'self.data.shape:'+str(self.data.shape)
+        print 'len(self.data):'+len(self.data)
+        print 'self.data.shape:'+str(self.data[0].shape)
         return [[(k, v.shape) for k, v in zip(self.data_name, self.data[i])] for i in xrange(len(self.data))]
 
     @property
