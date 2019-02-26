d007f877ba18a2c7b47860fd23cdcb2640dfc8af
qilei123 <qileimail123@gmail.com>
2019-02-26 14:56:10 -0500

    fda

--- lib/bbox/bbox.pyx
+++ lib/bbox/bbox.pyx
@@ -84,31 +84,32 @@ def bbox_overlaps_centerIns_cython(
         )
         query_box_center=[(query_boxes[k, 2] + query_boxes[k, 0])/2,(query_boxes[k, 3] + query_boxes[k, 1])/2]
         for n in range(N):
-            iw = (
-                min(boxes[n, 2], query_boxes[k, 2]) -
-                max(boxes[n, 0], query_boxes[k, 0]) + 1
-            )
-            if iw > 0:
-                ih = (
-                    min(boxes[n, 3], query_boxes[k, 3]) -
-                    max(boxes[n, 1], query_boxes[k, 1]) + 1
+            if boxes[n,4]==query_boxes[k,4]
+                iw = (
+                    min(boxes[n, 2], query_boxes[k, 2]) -
+                    max(boxes[n, 0], query_boxes[k, 0]) + 1
                 )
-                if ih > 0:
-                    ua = float(
-                        (boxes[n, 2] - boxes[n, 0] + 1) *
-                        (boxes[n, 3] - boxes[n, 1] + 1) +
-                        box_area - iw * ih
+                if iw > 0:
+                    ih = (
+                        min(boxes[n, 3], query_boxes[k, 3]) -
+                        max(boxes[n, 1], query_boxes[k, 1]) + 1
                     )
-                    overlaps[n, k] = iw * ih / ua
-                    if strategy==1:
-                        if query_box_center[0]<boxes[n,2] and query_box_center[0]>boxes[n,0]:
-                            if query_box_center[1]<boxes[n,3] and query_box_center[1]>boxes[n,1]:
-                                centerin_overlaps[n, k] = overlaps[n, k]                
-                    elif strategy==2:
-                        box_center=[(boxes[n, 2] - boxes[n, 0])/2,(boxes[n, 3] - boxes[n, 1])/2]
-                        if query_box_center[0]<boxes[n,2] and query_box_center[0]>boxes[n,0]:
-                            if query_box_center[1]<boxes[n,3] and query_box_center[1]>boxes[n,1]:
-                                if box_center[0]<query_boxes[k,2] and box_center[0]>query_boxes[k,0]:
-                                    if box_center[1]<query_boxes[k,3] and box_center[1]>query_boxes[k,1]:
-                                        centerin_overlaps[n,k] = overlaps[n, k]                
+                    if ih > 0:
+                        ua = float(
+                            (boxes[n, 2] - boxes[n, 0] + 1) *
+                            (boxes[n, 3] - boxes[n, 1] + 1) +
+                            box_area - iw * ih
+                        )
+                        overlaps[n, k] = iw * ih / ua
+                        if strategy==1:
+                            if query_box_center[0]<boxes[n,2] and query_box_center[0]>boxes[n,0]:
+                                if query_box_center[1]<boxes[n,3] and query_box_center[1]>boxes[n,1]:
+                                    centerin_overlaps[n, k] = overlaps[n, k]                
+                        elif strategy==2:
+                            box_center=[(boxes[n, 2] - boxes[n, 0])/2,(boxes[n, 3] - boxes[n, 1])/2]
+                            if query_box_center[0]<boxes[n,2] and query_box_center[0]>boxes[n,0]:
+                                if query_box_center[1]<boxes[n,3] and query_box_center[1]>boxes[n,1]:
+                                    if box_center[0]<query_boxes[k,2] and box_center[0]>query_boxes[k,0]:
+                                        if box_center[1]<query_boxes[k,3] and box_center[1]>query_boxes[k,1]:
+                                            centerin_overlaps[n,k] = overlaps[n, k]                
     return overlaps,centerin_overlaps
